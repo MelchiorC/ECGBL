39611089a428d1f244c110348a8ffab21d4a6f0b
choticIdea <lana.shurtugal@gmail.com>
2024-08-01 15:22:35 +0700

    Update kompos
    Refactor source code di pembuatan kompos. Pemisahan antara interactable script (dragndrop, mouse click dsb) dengan logic/behaviour inventory kompos

--- Assets/Scripts/Compost/CompostShower.cs
+++ Assets/Scripts/Compost/CompostShower.cs
@@ -13,7 +13,11 @@ public class CompostShower : MonoBehaviour
     public List<ItemData> craftingItem;
     public List<ItemData> craftable;
     public List<ItemData> craftingMaterial;
-
+    public List<GameObject> craftingSlots;
+    public List<GameObject> bagSlots;
+    public List<GameObject> resultSlots;
+    public int lastEmptyCraftingSlotId;
+    public Sprite defaultSprite;
     public Boolean CompostUI()
     {
         if (OnTrigger == true)
@@ -28,11 +32,14 @@ public class CompostShower : MonoBehaviour
             }
             if (InventoryManager.Instance.GetInventorySlots().Length > 0)
             {
+                
                 for (int i = 0; i < InventoryManager.Instance.GetInventorySlots().Length; i++)
                 {
+                    
                     if (InventoryManager.Instance.GetInventorySlots()[i].itemData != null)
                     {
                         craftingItem.Add(InventoryManager.Instance.GetInventorySlots()[i].itemData);
+                        Debug.Log(InventoryManager.Instance.GetInventorySlots().Length);
                         temp.Add(InventoryManager.Instance.GetInventorySlots()[i].itemData);
                     }
                 }
@@ -87,7 +94,7 @@ public class CompostShower : MonoBehaviour
         }
         else
         {
-            Debug.Log("test2");
+            Debug.Log("test2"+craftable.Count);
         }
     }
     public void CraftItems()
@@ -101,8 +108,10 @@ public class CompostShower : MonoBehaviour
         CraftItems();
     }
 
-    public void itemMovement()
+    public void itemMovement(bool isResultSlot, int destId, int originId, int operation, int trueDest )
     {
+        Debug.Log(originId);
+
         foreach (ItemData item in craftingItem)
         {
             if (item.compostmaterial > 0)
@@ -112,6 +121,27 @@ public class CompostShower : MonoBehaviour
         }
 
         craftingMaterial = new List<ItemData>();
+        //
+        switch (operation)
+        {
+            //0 from bag to crafting slot, 1 from crafting slot back to bag, 2 from result to bag
+            case 0: craftingSlots[lastEmptyCraftingSlotId].GetComponent<Image>().sprite = craftable[originId].thumbnail;
+                craftingSlots[lastEmptyCraftingSlotId].GetComponent<OnItemClick>().trueDest = originId;
+                bagSlots[originId].GetComponent<Image>().sprite = defaultSprite;
+                    if(lastEmptyCraftingSlotId < craftingSlots.Count - 1)
+                    {
+                        lastEmptyCraftingSlotId++;
+                    }
+                   break;
+            case 1:
+                craftingSlots[destId].GetComponent<Image>().sprite = defaultSprite;
+                bagSlots[trueDest].GetComponent<Image>().sprite = craftable[trueDest].thumbnail; ;
+                if (lastEmptyCraftingSlotId > 0)
+                {
+                    lastEmptyCraftingSlotId--;
+                }
+                break;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
